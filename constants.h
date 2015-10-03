68affd745374b100ec0517e100a85cc872fd01d3
Sourish Ghosh <ragnarok0211@gmail.com>
2015-10-03 16:39:22 +0530

    Change constants

--- constants.h
+++ constants.h
@@ -1,14 +1,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const float WORLD_WIDTH = 400;
-const float WORLD_HEIGHT = 400;
-const float BOT_RADIUS = 7;
-const float START_POS_X = 30;
-const float START_POS_Y = 30;
-const float END_POS_X = 363;
-const float END_POS_Y = 363;
-const float NODE_RADIUS = 1.5;
+const float WORLD_WIDTH = 400.0;
+const float WORLD_HEIGHT = 400.0;
+const float BOT_RADIUS = 7.0;
+const float START_POS_X = 30.0;
+const float START_POS_Y = 30.0;
+const float END_POS_X = 363.0;
+const float END_POS_Y = 363.0;
+const float NODE_RADIUS = 1.0;
 const float END_DIST_THRESHOLD = 15.0;
 
 #endif // CONSTANTS_H
